252c4cc535a855297b5f8720a77325e308d1faf4
Eric T <eithompson@uchicago.edu>
2014-12-17 16:41:44 -0600

    Complete Naive Bayes mini-project

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -27,8 +27,26 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.naive_bayes import GaussianNB
 
+t0=time()
 
+classifier = GaussianNB()
+classifier.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t1=time()
+predicted_labels=classifier.predict(features_test)
+print "prediction:", round(time()-t1, 3), "s"
+
+t1=time()
+
+number_right = 0
+for i in range(len(predicted_labels)):
+    number_right = number_right + (predicted_labels[i]==labels_test[i])
+
+accuracy = number_right/float(len(labels_test))
+print accuracy
 #########################################################
 
 
